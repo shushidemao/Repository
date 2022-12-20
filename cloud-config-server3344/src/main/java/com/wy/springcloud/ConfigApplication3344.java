05b49a9a542b195a8872c20915e82b418f58ed9a
wangyang <y_sr821@163.com>
2022-12-20 17:39:13 +0800

    增加springcloud config组件

---
+++ cloud-config-server3344/src/main/java/com/wy/springcloud/ConfigApplication3344.java
@@ -0,0 +1,15 @@
+package com.wy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ConfigApplication3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication3344.class,args);
+    }
+}
